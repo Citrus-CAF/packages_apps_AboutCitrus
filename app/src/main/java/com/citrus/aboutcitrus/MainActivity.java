b83bfeb65fde5182f84ec1cab425a71051164453
satyabrat-me <satyabrat.me@gmail.com>
2016-11-04 22:23:14 +0530

    citrus: Add more Menu options & Update strings

--- app/src/main/java/com/citrus/aboutcitrus/MainActivity.java
+++ app/src/main/java/com/citrus/aboutcitrus/MainActivity.java
@@ -1,15 +1,18 @@
 package com.citrus.aboutcitrus;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,14 +51,49 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_channel) {;
+        if (id == R.id.action_channel) {
             Intent intent = new Intent();
             intent.setAction(Intent.ACTION_VIEW);
             intent.setData(Uri.parse(getString(R.string.citrus_channel)));
             startActivity(intent);
             return true;
         }
+
+        if (id == R.id.action_wishbucket) {
+            String addresses[] = {"cool.leo.aditya@gmail.com", "aranha.joshwin@gmail.com", "satyabrat.me@gmail.com"};
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto", "Adarshmr1998@gmail.com", null));
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, addresses);
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Citrus-CAF: WishBucket");
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Your Wish");
+            startActivity(Intent.createChooser(emailIntent, "Send Email"));
+        }
+
+        if (id == R.id.action_bugreport) {
+            boolean appPresent = appCheck("com.nolanlawson.logcat");
+
+            if (appPresent) {
+                Intent intent = new Intent("com.nolanlawson.logcat.intents.LAUNCH");
+                startActivity(intent);
+            } else {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.nolanlawson.logcat"));
+                startActivity(i);
+            }
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean appCheck(String uri) {
+        PackageManager pm = getPackageManager();
+        boolean app_present;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_present = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            app_present = false;
+        }
+        return app_present;
     }
 
     public void launchCitrus(View view) {
