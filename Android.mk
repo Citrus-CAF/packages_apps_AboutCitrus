fc415bfb65f261ae8dfe582097ceae10e8ec4369
thelazyindian <satyabrat.me@gmail.com>
2017-02-27 07:22:33 +0000

    AboutCitrus: Fix Inline build

--- Android.mk
+++ Android.mk
@@ -12,19 +12,19 @@ LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v7-appcompat
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-cardview
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-annotations
+LOCAL_STATIC_JAVA_LIBRARIES +=android-support-v4
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := rebound:app/libs/facebook-rebound.jar
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += tastytoast:app/libs/tastytoast.jar
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += faboptions:app/libs/faboptions.jar
-
-LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
+LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main/java faboptions/src/main/java )
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res \
+    $(LOCAL_PATH)/faboptions/src/main/res \
+    $(LOCAL_PATH)/tastytoast/src/main/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/cardview/res \
     frameworks/support/design/res
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat:android.support.v7.cardview:android.support.design
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat:android.support.v7.cardview:android.support.design:com.joaquimley.faboptions
 
 include $(BUILD_PACKAGE)
 
