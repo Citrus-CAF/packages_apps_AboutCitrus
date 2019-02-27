37b8f88ac4e023c1c64517004fdeef07f2169c02
Jashaswee Jena <j.ishujena2000@gmail.com>
2019-02-27 23:33:38 +0530

    Migrate to AndroidX

--- app/src/main/java/com/citrus/aboutcitrus/Helpers/Corner.java
+++ app/src/main/java/com/citrus/aboutcitrus/Helpers/Corner.java
@@ -1,6 +1,6 @@
 package com.citrus.aboutcitrus.Helpers;
 
-import android.support.annotation.IntDef;
+import androidx.annotation.IntDef;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
